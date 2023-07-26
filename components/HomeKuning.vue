f748221cc11f01773a624e3914033df6e782790c
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-07-27 01:30:20 +0700

    1 file modified, 1 added

---
+++ components/HomeKuning.vue
@@ -0,0 +1,92 @@
+<template>
+ <section class="relative -mt-24">
+      <div class="container kunings w-full">
+        <!-- Statistics -->
+        <div
+          class="
+            mb-24
+            overflow-hidden
+            mx-auto
+            max-w-screen-md
+            rounded-xl
+            border-2 border-yellow-400
+            bg-gradient-to-br
+            from-yellow-400
+            to-yellow-500
+            shadow-md
+          "
+        >
+          <div class="grid grid-cols-2 md:grid-cols-4">
+            <div
+              class="
+                py-5
+                sm:py-12
+                text-center
+                align-middle
+                duration-300
+                hover:scale-110 hover:bg-black/5
+              "
+            >
+              <p class="px-5 sm:px-12">
+                <b
+                  ><span class="text-5xl">40</span><br />
+                  <span class="">tahun telah berdiri</span></b
+                >
+              </p>
+            </div>
+            <div
+              class="
+                py-5
+                sm:py-12
+                text-center
+                align-middle
+                duration-300
+                hover:scale-110 hover:bg-black/5
+              "
+            >
+              <p class="px-5 sm:px-12" style="text-align: center">
+                <b
+                  ><span class="text-5xl">63</span><br />
+                  <span class="">cabang di indonesia</span></b
+                >
+              </p>
+            </div>
+            <div
+              class="
+                py-5
+                sm:py-12
+                text-center
+                align-middle
+                duration-300
+                hover:scale-110 hover:bg-black/5
+              "
+            >
+              <p class="px-5 sm:px-12" style="text-align: center">
+                <b
+                  ><span class="text-5xl">7T</span><br />
+                  <span class="">total triliun aset</span></b
+                >
+              </p>
+            </div>
+            <div
+              class="
+                py-5
+                sm:py-12
+                text-center
+                align-middle
+                duration-300
+                hover:scale-110 hover:bg-black/5
+              "
+            >
+              <p class="px-5 sm:px-12" style="text-align: center">
+                <b
+                  ><span class="text-5xl">3K</span><br />
+                  <span class="">lebih karyawan</span></b
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>

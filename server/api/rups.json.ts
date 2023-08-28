28967921b8463a5692e668e8a4eda3523afefd5c
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-08-28 14:20:03 +0700

    1 file modified, 1 added

---
+++ server/api/rups.json.ts
@@ -0,0 +1,242 @@
+[
+  {
+    "tahun": "2023",
+    "daftars": [
+      {
+        "judul": "Pengumuman",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Pemanggilan",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Surat Kuasa",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Tata Tertib",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Informasi Dividen",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "tahun": "2022",
+    "daftars": [
+      {
+        "judul": "Pengumuman",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Pemanggilan",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Surat Kuasa",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Tata Tertib",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Informasi Dividen",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "tahun": "2021",
+    "daftars": [
+      {
+        "judul": "Pengumuman",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Pemanggilan",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Surat Kuasa",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Tata Tertib",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Informasi Dividen",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "tahun": "2020",
+    "daftars": [
+      {
+        "judul": "Pengumuman",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Pemanggilan",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Surat Kuasa",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Tata Tertib",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      },
+      {
+        "judul": "Informasi Dividen",
+        "download": [
+          {
+            "to": "/profile#tentang"
+          },
+          {
+            "to": "/prof"
+          }
+        ]
+      }
+    ]
+  }
+]

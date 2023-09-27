dd140786c007e57861d3e9c60d118a19e07573d2
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-09-28 03:40:58 +0700

    dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto max-w-screen-xl">
-    <div class="flex gap-10 p-5 h-48">
-      <div class="w-8/12">
+    <div class="flex flex-col sm:flex-row gap-10 p-5 h-48">
+      <div class="sm:w-8/12">
         <div
           class="
             bg-blue-600
@@ -25,84 +25,156 @@
               <p class="capitalize font-bold text-xl mt-2">
                 rachmat alpriansya
               </p>
-              <p class="capitalize text-sm font-bold">1234567890123</p>
+              <p class="capitalize text-sm font-bold py-1">
+                <span class="px-2 mr-2 py-1 bg-green-500 rounded text-xs"
+                  >lancar</span
+                >1234567890123
+              </p>
             </div>
           </div>
-          <div class="bg-white p-8 rounded-lg shadow-xl px-12">
+          <div class="hidden sm:grid bg-white p-8 rounded-lg shadow-xl px-12">
             <p class="capitalize font-bold text-4xl">32</p>
             <p class="capitalize text-sm">angsuran <br />tersisa</p>
           </div>
         </div>
         <div>
-          <h4 class="uppercase font-bold text-md text-blue-600 mb-2">
-            Detail Kredit
-          </h4>
+          <div>
+            <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
+              <div class="p-4 bg-white hover:shadow-lg hover:text-blue-600 text-center py-6 rounded-xl">
+                <div>
+                  <p class="capitalize text-xs text-black">OTR Kendaraan</p>
+                  <h4 class="capitalize font-semibold text-xl text-blue-600">420.000.000</h4>
+                </div>
+              </div>
+              <div class="p-4 bg-white hover:shadow-lg hover:text-blue-600 text-center py-6 rounded-xl">
+                <div>
+                  <p class="capitalize text-xs text-black">Uang Muka</p>
+                  <h4 class="capitalize font-semibold text-xl text-blue-600">46.500.000</h4>
+                </div>
+              </div>
+              <div class="p-4 bg-white hover:shadow-lg hover:text-blue-600 text-center py-6 rounded-xl">
+                <div>
+                  <p class="capitalize text-xs text-black">Angsuran</p>
+                  <h4 class="capitalize font-semibold text-xl text-blue-600">6.500.000</h4>
+                </div>
+              </div>
+              <div class="p-4 bg-white hover:shadow-lg hover:text-blue-600 text-center py-6 rounded-xl">
+                <div>
+                  <p class="capitalize text-xs text-black">tenor</p>
+                  <h4 class="capitalize font-semibold text-xl text-blue-600">24 bulan</h4>
+                </div>
+              </div>
+            </div>
+          
+            <div class="grid sm:grid-cols-2 gap-4 mt-4">
+              <div class="bg-white hover:shadow-lg hover:text-blue-600 rounded-xl">
+                <div class="space-y-4 p-4">
+                  <h4 class="capitalize font-semibold text-2xl text-blue-600">
+                    All New Avanza Velos CVT
+                  </h4>
+                  <hr class="my-2">
+                  <div
+                    class="
+                      grid grid-cols-2
+                      gap-5
+                      
+                      items-start
+                      justify-between
+                      py-2
+                    "
+                  >
+                    <div>
+                      <p class="capitalize text-xs">Brand</p>
+                      <p class="capitalize font-semibold text-sm">Toyota</p>
+                    </div>
+                    <div>
+                      <p class="capitalize text-xs">Tahun</p>
+                      <p class="capitalize font-semibold text-sm">2023</p>
+                    </div>
+                    <div>
+                      <p class="capitalize text-xs">Warna</p>
+                      <p class="capitalize font-semibold text-sm">
+                        Metalik Biru
+                      </p>
+                    </div>
+                    <div>
+                      <p class="capitalize text-xs">Asuransi</p>
+                      <p class="capitalize font-semibold text-sm">TLO</p>
+                    </div>
+                    <div class="col-span-2">
+                      <p class="capitalize text-xs">Dealer/Showroom</p>
+                      <p class="capitalize font-semibold text-sm">
+                        Maju Sakti Motor Group
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-          <div class="grid grid-cols-4 gap-4">
-            <div class="col-span-2 p-5 bg-white">
-              <div class="flex gap-4 items-top justify-between w-full">
-                <div>
-                  <img
-                    src="https://tairo.cssninja.io/img/avatars/2.svg"
-                    class="rounded-full w-12"
-                  />
-                </div>
-                <div>
-                  <p class="capitalize text-sm">Tipe Kendaraan</p>
-                  <p class="capitalize font-semibold text-xl">
-                    All New Avanza Velos CVT Type
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="p-5 bg-white">
-              <div>
-                <p class="capitalize text-sm">Merek</p>
-                <p class="capitalize font-semibold text-xl">Toyota</p>
-              </div>
-            </div>
-            <div class="p-5 bg-white">
-              <div>
-                <p class="capitalize text-sm">Tahun</p>
-                <p class="capitalize font-semibold text-xl">Late 2023</p>
-              </div>
-            </div>
-          </div>
-           <div class="grid grid-cols-4 gap-4 mt-4">
-            <div class="p-5 bg-white">
-              <div>
-                  <p class="capitalize text-sm">OTR Kendaraan</p>
-                  <p class="capitalize font-semibold text-xl">
-                   420.000.000
-                  </p>
-                </div>
-            </div>
-            <div class="p-5 bg-white">
-              <div>
-                <p class="capitalize text-sm">Uang Muka</p>
-                <p class="capitalize font-semibold text-xl">46.500.000</p>
-              </div>
-            </div>
-            <div class="p-5 bg-white">
-              <div>
-                <p class="capitalize text-sm">Angsuran</p>
-                <p class="capitalize font-semibold text-xl">6.500.000</p>
-              </div>
-            </div>
-            <div class="p-5 bg-white">
-              <div>
-                <p class="capitalize text-sm">Tenor</p>
-                <p class="capitalize font-semibold text-xl">48 Bulan</p>
+              <div class="p-4 bg-white hover:shadow-lg hover:text-blue-600 rounded-xl">
+                 <div>
+                    <div
+                      class="
+                        text-xl
+                        uppercase
+                        font-bold
+                        bg-green-500
+                        text-center
+                        py-2
+                        rounded-md
+                        mb-2
+                      "
+                    >
+                      MOBIL BARU
+                    </div>
+                    <div class="flex items-end justify-between py-2">
+                      <div>
+                        <p class="capitalize text-sm font-bold">Nopol</p>
+                      </div>
+                      <div>
+                        <p class="capitalize text-sm">B 1233 CD</p>
+                      </div>
+                    </div>
+                    <div class="flex items-end justify-between py-2">
+                      <div>
+                        <p class="capitalize text-sm font-bold">Cabang</p>
+                      </div>
+                      <div>
+                        <p class="capitalize text-sm">Tangerang 4</p>
+                      </div>
+                    </div>
+                    <div class="flex items-end justify-between py-2">
+                      <div>
+                        <p class="capitalize text-sm font-bold">Marketing</p>
+                      </div>
+                      <div>
+                        <p class="capitalize text-sm">Januar Rusdi</p>
+                      </div>
+                    </div>
+                    <div class="flex items-end justify-between py-2">
+                      <div>
+                        <p class="capitalize text-sm font-bold">Kontrak</p>
+                      </div>
+                      <div>
+                        <p class="capitalize text-sm">14 Januari 2023</p>
+                      </div>
+                    </div>
+                    <div class="flex items-end justify-between py-2">
+                      <div>
+                        <p class="capitalize text-sm font-bold">Promo</p>
+                      </div>
+                      <div>
+                        <p class="capitalize text-sm">tidak ada</p>
+                      </div>
+                    </div>
+                  </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="w-4/12 sticky fixed">
-        <div class="bg-white rounded-xl flex-col p-5">
-          <p class="capitalize font-bold text-blue-600 mb-4">
-            ringkasan Kredit
-          </p>
+      <div class="sm:w-4/12">
+        <div class="bg-white rounded-xl flex-col p-2">
           <div
             class="
               bg-blue-500
@@ -116,7 +188,9 @@
             "
           >
             <p class="capitalize text-sm">Tagihan</p>
-            <p class="capitalize font-bold text-3xl py-3">Rp 6,500,000</p>
+            <h4 class="capitalize font-bold text-4xl text-white py-3">
+              Rp 6,500,000
+            </h4>
             <div class="flex items-end justify-between">
               <div>
                 <p class="capitalize text-xs mt-8">Angsuran ke</p>
@@ -128,24 +202,51 @@
               </div>
             </div>
           </div>
-          <div>
-            <div class="flex items-end justify-between px-5 py-2">
-              <div>
-                <p class="capitalize text-sm font-bold">Status</p>
+          <div class="px-2 pb-2">
+            <div>
+              <div class="flex items-end justify-between px-5 py-2">
+                <div>
+                  <p class="capitalize text-sm font-bold">Status</p>
+                </div>
+                <div>
+                  <p class="capitalize text-sm">Belom terbayar</p>
+                </div>
               </div>
-              <div>
-                <p class="capitalize text-sm">Belom terbayar</p>
+              <div class="flex items-end justify-between px-5 py-2">
+                <div>
+                  <p class="capitalize text-sm font-bold">Jatuh Tempo</p>
+                </div>
+                <div>
+                  <p class="capitalize text-sm">21 Januari</p>
+                </div>
+              </div>
+              <div class="flex items-end justify-between px-5 py-2">
+                <div>
+                  <p class="capitalize text-sm font-bold">Denda</p>
+                </div>
+                <div>
+                  <p class="capitalize text-sm">0</p>
+                </div>
+              </div>
+              <div class="flex items-end justify-between px-5 py-2">
+                <div>
+                  <p class="capitalize text-sm font-bold">Produk</p>
+                </div>
+                <div>
+                  <p class="capitalize text-sm">Mobil Bekas</p>
+                </div>
               </div>
             </div>
-            <div class="flex items-end justify-between px-5 py-2">
+            <div class="border-dashed border-b my-2 border-gray-600 ..."></div>
+            <div class="flex items-end justify-between px-5 pt-2">
               <div>
-                <p class="capitalize text-sm font-bold">Jatuh Tempo</p>
+                <p class="capitalize text-sm font-bold">Angsuran</p>
               </div>
               <div>
-                <p class="capitalize text-sm">21 Januari</p>
+                <p class="capitalize text-sm">6,500,000</p>
               </div>
             </div>
-            <div class="flex items-end justify-between px-5 py-2">
+            <div class="flex items-end justify-between px-5 pb-2">
               <div>
                 <p class="capitalize text-sm font-bold">Denda</p>
               </div>
@@ -155,39 +256,30 @@
             </div>
             <div class="flex items-end justify-between px-5 py-2">
               <div>
-                <p class="capitalize text-sm font-bold">Produk</p>
+                <p class="uppercase text-sm font-bold">total Tagihan</p>
               </div>
               <div>
-                <p class="capitalize text-sm">Mobil Bekas</p>
+                <p class="capitalize text-sm font-bold">6,500,000</p>
               </div>
             </div>
+            <button
+              class="
+                capitalize
+                text-sm
+                font-bold
+                bg-blue-500
+                w-full
+                text-center
+                p-5
+                rounded-lg
+                mt-4
+                shadow-lg
+                text-white
+              "
+            >
+              Bayar Sekarang
+            </button>
           </div>
-          <div class="border-dashed border-b my-5 border-gray-600 ..."></div>
-          <div class="flex items-end justify-between px-5 py-2">
-            <div>
-              <p class="capitalize text-sm font-bold">total Tagihan</p>
-            </div>
-            <div>
-              <p class="capitalize text-sm">6,500,000</p>
-            </div>
-          </div>
-          <button
-            class="
-              capitalize
-              text-sm
-              font-bold
-              bg-blue-500
-              w-full
-              text-center
-              p-5
-              rounded-lg
-              mt-4
-              shadow-lg
-              text-white
-            "
-          >
-            Bayar Sekarang
-          </button>
         </div>
       </div>
     </div>

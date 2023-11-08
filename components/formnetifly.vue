3897969ea036b8f35b596b4c5c5ef417029ce313
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-11-08 16:38:11 +0700

    1 file modified

--- components/formnetifly.vue
+++ components/formnetifly.vue
@@ -1,148 +1,43 @@
 <template>
-  <div>
-    <form
-      name="contact"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      @submit="handleSubmit"
-    >
-      <div hidden>
-        <label>
-          Don’t fill this out: <input name="bot-field" />
-        </label>
-      </div>
-      <div>
-        <label>Name:
-          <input 
-            type="text" 
-            name="name"
-            v-model="formData.name"
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>Email:
-          <input 
-            type="email" 
-            name="email"
-            v-model="formData.email"
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>Message:
-          <textarea 
-            name="message"
-            v-model="formData.message"
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label class="subtext" for="checkbox">
-          <input type="checkbox" v-model="checked" />
-          I certify that this message is not full of odeous lies
-        </label>
-      </div>
-      <div>
-        <button 
-        class="p-4 bg-blue-400"
-          type="submit" 
-          :disabled="!checked"
-        >
-          Send
-        </button>
-      </div>
+    <form @submit.prevent="submitForm">
+      <input type="text" name="name" v-model="name"/>
+      <input type="email" name="email"  v-model="email"/>
+      <textarea name="message" v-model="message"></textarea>
+      <button type="submit">Send Message</button>
     </form>
-    <div 
-      class="alert"
-      :class="formAlert.class"
-      v-if="formAlert.show"
-    >
-      {{ formAlert.msg }}
-    </div>
-  </div>
-</template>
+  </template>
 
-<script>
-function encode (data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
-export default {
-  data () {
-    return {
-      formData: {
-        name: '',
-        email: '',
-        message: ''
+  <script>
+  const WEB3FORMS_ACCESS_KEY = "8ff22112-8c74-4419-8f79-0213f1a5ba99";
+
+  export default {
+    data() {
+      return {
+        name: "",
+        email: "",
+        message: "",
+      };
+    },
+    methods: {
+      async submitForm() {
+        const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            access_key: WEB3FORMS_ACCESS_KEY,
+            name: this.name,
+            email: this.email,
+            message: this.message,
+          }),
+        });
+        const result = await response.json();
+        if (result.success) {
+          console.log(result);
+        }
       },
-      checked: false,
-      formAlert: {
-        show: false,
-        msg: '',
-        class: ''
-      }
-    }
-  },
-  methods: {
-    handleSubmit (e) {
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({ 'form-name': 'contact', ...this.formData })
-      })
-        .then(res => {
-          if (res.status === 200) {
-            this.formData = {
-              name: '',
-              email: '',
-              message: ''
-            }
-            this.formAlert = {
-              show: true,
-              msg: 'Thank you for your message.  We will reply to you shortly.',
-              class: 'success'
-            }
-          } else {
-            this.formAlert = {
-              show: true,
-              msg: `There was an error: ${res.status} ${res.statusText}. Try again or email us directly.`,
-              class: 'error'
-            }
-          }
-          this.checked = false
-        })
-        .catch(error => {
-          this.formAlert = {
-            show: true,
-            msg: `${error.message}. Try again or email us directly.`,
-            class: 'error'
-          }
-        })
-
-      e.preventDefault()
-    }
-  }
-}
-</script>
-
-<style scoped>
-form {
-  margin: 1rem;
-}
-.alert {
-  margin: 1rem;
-  padding: 1rem;
-  color: white;
-}
-.success {
-  background-color: green;
-}
-.error {
-  background-color: red;
-}
-</style>
+    },
+  };
+  </script>

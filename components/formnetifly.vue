16c96986448f6655d8cd3564112d7e4bce0993c9
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-11-25 02:37:10 +0700

    Update formnetifly.vue

--- components/formnetifly.vue
+++ components/formnetifly.vue
@@ -1,38 +1,58 @@
 <template>
-  <form
-    name="ask-question"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
+  <div class="feedback-form">
+    <form
+      netlify
+      netlify-honeypot
+      name="feedback"
+      method="POST"
+      action="/success"
     >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
-    ...
-    <button>Submit</button>
-  </form>
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you’re human: <input name="bot-field" />
+        </label>
+      </p>
+      <input type="hidden" name="form-name" value="feedback" />
+      <label for="name">Name</label>
+      <input id="name" type="text" name="name" />
+      <label for="email">Email</label>
+      <input id="email" type="email" name="email" required />
+      <label for="feedback">What is your feedback?</label>
+      <textarea id="feedback" wrap="soft" name="feedback" required></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
-<script>
-export default {
-  name: "QAForm",
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
-    }
-  },
-  data () {
-    return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
-    }
-  }
+
+<style scoped>
+.feedback-form {
+  margin: 40px 40px;
+  min-width: 250px;
+  width: 45%;
 }
-</script>
+input,
+textarea,
+button {
+  font-size: inherit;
+  margin: 15px 0;
+  padding: 12px 20px;
+  width: 100%;
+}
+
+button {
+  background-color: teal;
+  border: 1px solid black;
+  color: white;
+  width: 50%;
+}
+
+.hidden {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+</style>

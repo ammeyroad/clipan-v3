de419fdb1d26a2473ce1234701b86d8cd3ffd883
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-11-25 01:44:03 +0700

    Update index form.vue

--- pages/dummy/index.vue
+++ pages/dummy/index.vue
@@ -45,6 +45,17 @@
         </div>
       </div>
     </section>
+    <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <section
       class="
         flex
